8262197b58ce4e183949ceb20e4be8a0c5f2df48
msantares <ms.antares@mail.ru>
2016-10-27 14:00:54 +0300

    Добавлено определение местоположения посетителя через Geolocation

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,21 +16,53 @@ export class AppComponent {
         this.cities = (localStorage.getItem('cities')) ?
             JSON.parse(localStorage.getItem('cities')) :
             [];
+
+        this.addUserCity();
     }
 
 
-    addCity(name: string ) {
-        let weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lang=ru&units=metric&APPID=7912fc72c98b9e9e6659c3c7095a5614&q='+name;
+    addUserCity (){
+        let that = this; // костыль
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+
+        function success(pos) {
+            var crd = pos.coords;
+            let str = '&lat='+crd.latitude+'&lon='+crd.longitude+'';
+            that.addCity(str);
+        };
+
+        function error(err) {
+            console.warn('ERROR(' + err.code + '): ' + err.message);
+        };
+
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+
+
+    addCity(query: string ) {
+        let weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lang=ru&units=metric&APPID=7912fc72c98b9e9e6659c3c7095a5614'+query;
         return this.http.get(weatherUrl)
             .toPromise()
             .then(response => {
-                let cityData = {
-                    "name": "" + name + "",
-                    "windSpeed": "" + response.json().wind.speed + "",
-                    "temperature": "" + response.json().main.temp + ""
-                };
-                this.cities.push(cityData);
-                localStorage.setItem('cities', JSON.stringify(this.cities));
+                let hasCity = false;
+                this.cities.forEach(function(item) {
+                    if(item.name === response.json().name)
+                        hasCity = true;
+                });
+
+                if(!hasCity){
+                    let cityData = {
+                        "name": "" + response.json().name + "",
+                        "windSpeed": "" + response.json().wind.speed + "",
+                        "temperature": "" + response.json().main.temp + ""
+                    };
+                    this.cities.push(cityData);
+                    localStorage.setItem('cities', JSON.stringify(this.cities));
+                }
             })
             .catch(this.handleError);
     }
